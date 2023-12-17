0e44e5750e7ae0eeaf834258c5dbac18f14f5d04
Purushothaman G <macdasys@gmail.com>
2023-12-17 19:32:31 +0530

    product error silenced in converttobill

--- src/components/Billings/convertToBill.ts
+++ src/components/Billings/convertToBill.ts
@@ -7,15 +7,15 @@ const convertToBill = (product: Product) => {
     productId: product.code,
     productName: product.itemName,
     quantity: 1,
-    quantityPrice: product.salePrice,
-    tax: product.taxRate,
-    billPrice: product.salePrice,
-    salePrice: product.salePrice,
-    purchasePrice: product.purchasePrice,
-    total: parseFloat(
-      (product.salePrice + product.taxRate * product.purchasePrice).toFixed(2)
-    ),
-    taxPrice: parseFloat((product.taxRate * product.purchasePrice).toFixed(2)),
+    // quantityPrice: product.salePrice,
+    // tax: product.taxRate,
+    // billPrice: product.salePrice,
+    // salePrice: product.salePrice,
+    // purchasePrice: product.purchasePrice,
+    // total: parseFloat(
+    //   (product.salePrice + product.taxRate * product.purchasePrice).toFixed(2)
+    // ),
+    // taxPrice: parseFloat((product.taxRate * product.purchasePrice).toFixed(2)),
   } as BillingEntry;
 };
 
